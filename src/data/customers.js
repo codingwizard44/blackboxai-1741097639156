2891bb5481efa4730cdda7496abf779807daab6a
codingwizard44 <123013244+codingwizard44@users.noreply.github.com>
2025-03-04 19:44:10 +0530

    Add src/data/customers.js 1741097650068

---
+++ src/data/customers.js
@@ -0,0 +1,25 @@
+export const customers = {
+  "result": [
+    {
+      "datasource_id": "10005_1",
+      "parent_datasource_id": "10716_1",
+      "status": "Approved",
+      "name": "Lorem ipsum dolor sit amet",
+      "name_2": "Consectetur adipiscing elit",
+      "store_locations": [
+        {
+          "datasource_id": "10005_1_storeloc",
+          "name": "Sed do eiusmod tempor",
+          "address_line_1": "1 Parsons Road,",
+          "address_line_2": "Washington,",
+          "country_code": "GB",
+          "region": null,
+          "zip": "GB-NE371EZ",
+          "city": "Washington",
+          "county": null
+        }
+      ]
+    },
+    // ... rest of the customers data
+  ]
+}
